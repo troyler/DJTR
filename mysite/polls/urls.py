0651dd393aceee33dffc5f4c9a148502b3885fda
troyler <tmreinert22@gmail.com>
2024-01-03 22:30:24 -0500

    starting google photos API add in

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-
+#if the preceding url path matches '/polls' followin the domain name,
+#the main urls.py file will strip the remainder of the path and send it here
 from . import views
 
+
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>", views.DetailView.as_view(), name="detail"), # ex: /polls/5/results/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),# ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
     
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .models import Question
+from .models import Question, Choice
 from django.http import Http404
+from django.urls import reverse
+from django.views import generic
 
 #Each view is responsible for doing one of two things:
 # 1.) returning an HttpResponse object containing the content for the requested page
 # 2.) or raising an exception such as Http404. The rest is up to you.
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"  #use this to redefine the autodefined variable 'question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    template_name = "polls/detail.html"
+    model = Question
+
+
+class ResultsView(generic.DetailView):
+    template_name = "polls/results.html"
+    model = Question
+
 
 #t’s a very common idiom to load a template, fill a context
 # and return an HttpResponse object with the result of the 
@@ -34,7 +47,7 @@ def index(request):
 # as the second argument, and a dictionary as its third (optional) arg
 #returns an HTTpRespnse of the givene template rendered with the content
 
-def detail(request, question_id):
+"""def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
@@ -43,9 +56,29 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question" : question})
+"""
 
+def vote(request, question_id):   #at detail page, the vote form is submitted as a post request, calling the url with the polls:vote and the question.id and posting the request object containing the choice id
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])   #visit the question entry in the database and use the contents of the input to gather the info
+        #from the html page <input type="radio" name="choice" id="choice{{ forloop.counter }}" value="{{ choice.id }}">
+    except (KeyError, Choice.DoesNotExist):
+        #redisplaying the voting form if an error occurs
+        return render(
+            request,    
+            "poll/detail.html",   
+            {"question" : question, 
+             "error_message" : "You didn't select a choice.",
+             },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        #Always return an HttpRespnseRedirect after successfully dealing with post data 
+        #prevents data from being posted twice if a user hits the back button 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+        return HttpResponseRedirect(reverse("polls:results", args = (question.id,)))
+        
